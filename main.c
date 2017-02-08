cbee2d5575463aa269868160c813d7006badd025
ivana <ivana@ivanas-MacBook-Air.local>
2017-02-08 18:44:01 +0700

    finished

--- main.c
+++ main.c
@@ -13,10 +13,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define length 10
+#define length 40
 unsigned char array[length];
-unsigned short place = 0;
+unsigned char comparator[1];
+unsigned short currFree = 0;
+unsigned short leftBefore = 0;
+unsigned short rightBefore = 0;
 
 struct Node{
     unsigned char key;
@@ -30,12 +34,14 @@ void putIndex (unsigned short idx, unsigned short start);
 void writeArrayToFile();
 unsigned char* readFile();
 void printArray (unsigned char* test);
-void insertChar (char key);
+void insertChar (char key, int lastMatch);
+void insertString(char* input);
 
 int main(int argc, char** argv) {
     
-    insertChar('a');
     
+    insertString("udin");
+    insertString("uda");
     writeArrayToFile();
     unsigned char *test = readFile();
     
@@ -76,11 +82,52 @@ void printArray(unsigned char* test){
         printf("isi test[%d]: %u \n", i, test[i]);
 }
 
-void insertChar(char key){
-    printf("free before: %d \n", place);
-    array[place] = key;
-    putIndex(0, place+1);
-    place += 5;
-    printf("free after: %d \n", place);
+void insertChar(char key, int lastMatch){
+    printf("free before: %d \n", currFree);
+    array[currFree] = key;
+    putIndex(0, currFree+1);
+    putIndex(0, currFree+3);
+    
+    if(currFree > 0 && lastMatch == -1){
+        putIndex(currFree, currFree-4);
+    }
+    if(currFree > 0 && lastMatch != -1){
+        putIndex(currFree, lastMatch+3);
+    }
+    
+    currFree += 5;
+    printf("free after: %d \n", currFree);
 }
 
+void insertString(char* input){
+    int checker = 0;
+    int idxInput = 0;
+    char stop='0';
+    
+    while(stop == '0'){
+        comparator[0] = input[idxInput];
+        printf("array[%d]: %u -- input[%d]: %u \n", checker, array[checker], idxInput, comparator[0]);
+        if((char) array[checker] == comparator[0]) {
+            int nextIdx = getIndex(checker+1);
+            if(nextIdx != -1)
+            {
+                checker = getIndex(checker+1);
+                idxInput++;
+            }
+            else {
+                printf("checker: %d \n", checker);
+                stop = '1';
+            }
+        } else{
+            printf("checker: %d \n", checker);
+            stop = '1';
+        }
+    }
+    
+    int inputSize = (int) strlen(input);
+    int i=idxInput;
+    insertChar(input[i], checker);
+    for(i=idxInput+1; i<inputSize; i++){
+        insertChar(input[i], -1);
+    }
+}
