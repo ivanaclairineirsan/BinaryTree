9772ee7cab8f9567e3aec4cf0038990477087cd9
ivanaclairineirsan <ivana.clairine@gmail.com>
2017-02-24 10:55:08 +0700

    binaryTree done

--- main.c
+++ main.c
@@ -15,40 +15,52 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define length 40
+int length=100;
 #define COUNT 10
-unsigned char array[length];
+unsigned char* array;
+unsigned char* read;
 unsigned char comparator[1];
 unsigned short currFree = 0;
 unsigned short currPrint = 0;
 
-struct Node{
-    unsigned char key;
-    unsigned short left;
-    unsigned short right;
-};
+int isValidTree = 1;
 
 unsigned short getIndex (unsigned short idx);
 
 void putIndex (unsigned short idx, unsigned short start);
 void writeArrayToFile();
-unsigned char* readFile();
-void printArray (unsigned char* test);
+void readFile();
+void printArray ();
 void printTree(unsigned short index, int space);
 void insertChar (char key, int lastMatch);
 void insertString(char* input);
 
 int main(int argc, char** argv) {
-    
+	array = (unsigned char*) malloc (length * sizeof(unsigned char));
+
+	int i;
+	for(i=0; i<length; i++){
+		array[i] = 0;
+	}
     
     insertString("uda");
+    // printf("sizeOf(array): %d\n", sizeof(array));
     insertString("udin");
-    writeArrayToFile();
-    unsigned char *test = readFile();
+    // printf("sizeOf(array): %d\n", sizeof(array));
+    insertString("abah");
+    insertString("saya");
+    insertString("makan");
+    insertString("lala");
 
-    printTree(0,0);
-    
-    return (EXIT_SUCCESS);
+    if(isValidTree)
+    {
+    	writeArrayToFile();
+	    readFile();
+
+	    // printTree(0,0);
+	}
+
+	return (EXIT_SUCCESS);
 }
 
 unsigned short getIndex (unsigned short idx){
@@ -62,27 +74,38 @@ void putIndex (unsigned short idx, unsigned short start){
 
 void writeArrayToFile(){
     FILE *file = fopen("arrayFile.txt", "wb");
-    fwrite(array, sizeof(unsigned char), sizeof(array), file);
+    fwrite(array, sizeof(unsigned char), length, file);
     fclose(file);
+    printf("File saved! File name: arrayFile.txt \n");
+    printf("fileSize (write): %d\n", fsize("arrayFile.txt"));
 }
 
-unsigned char* readFile(){
-    printf("test");
-    unsigned char read[length];
+void readFile(){
+    // printf("fileSize (read): %d\n", fsize("arrayFile.txt"));
     FILE *file = fopen("arrayFile.txt", "rb");
-    fread(read, sizeof(unsigned char), sizeof(read), file );
+    int fileSize = fsize("arrayFile.txt");
+    read = (unsigned char*) malloc (sizeof(unsigned char) * fileSize);
+    fread(read, sizeof(unsigned char), fileSize, file );
     fclose(file);
-    
-    int i;
-    printf(" --- Print isi file --- \n");
-    printArray(read);
-    return read;
+    printf("--- READ FILE --- \n--- Print isi file --- \n");
+    printArray();
 }
 
-void printArray(unsigned char* test){
+int fsize(char* file)
+{
+    FILE * f = fopen(file, "r");
+    fseek(f, 0, SEEK_END);
+    unsigned long len = (unsigned long)ftell(f);
+    fclose(f);
+    return len;
+}
+
+void printArray(){
     int i;
     for(i=0; i< currFree; i++)
-        printf("arr[%d]: %u \n", i, test[i]);
+    {
+    	printf("arr[%d]: %u \n", i, read[i]);
+    }   
 }
 
 void printTree(unsigned short index, int space){
@@ -110,7 +133,7 @@ void printTree(unsigned short index, int space){
     int i;
     for (i = COUNT; i < space; i++)
         printf("-");
-    printf("%d\n", array[index]);
+    printf("%c\n", (char) array[index]);
  
 
     // Process left child
@@ -123,20 +146,29 @@ void printTree(unsigned short index, int space){
 }
 
 void insertChar(char key, int lastMatch){
-    printf("free before: %d \n", currFree);
-    array[currFree] = key;
-    putIndex(0, currFree+1);
-    putIndex(0, currFree+3);
-    
-    if(currFree > 0 && lastMatch == -1){
-        putIndex(currFree, currFree-4);
-    }
-    if(currFree > 0 && lastMatch != -1){
-        putIndex(currFree, lastMatch+3);
-    }
-    
-    currFree += 5;
-    printf("free after: %d \n", currFree);
+    // printf("free before: %d \n", currFree);
+    if(currFree < length)
+    {
+    	array[currFree] = key;
+	    putIndex(0, currFree+1);
+	    putIndex(0, currFree+3);
+	    
+	    if(currFree > 0 && lastMatch == -1){
+	        putIndex(currFree, currFree-4);
+	    }
+	    if(currFree > 0 && lastMatch != -1){
+	        putIndex(currFree, lastMatch+3);
+	    }
+	    
+	    currFree += 5;
+	 }
+	 else{
+	   length = length*2;
+	   printf("Re-alloc! size now: %d\n", length);
+	   array = (unsigned char *) realloc(array, length);
+	   insertChar(key, lastMatch);
+	 }
+    // printf("free after: %d \n", currFree);
 }
 
 void insertString(char* input){
@@ -146,8 +178,8 @@ void insertString(char* input){
     
     while(stop == '0'){
         comparator[0] = input[idxInput];
-        printf("array[%d]: %u -- input[%d]: %u \n", checker, array[checker], idxInput, comparator[0]);
-        if((char) array[checker] == comparator[0]) {
+        // printf("array[%d]: %u -- input[%d]: %u \n", checker, array[checker], idxInput, comparator[0]);
+        if((unsigned char) array[checker] == comparator[0]) {
             int nextIdx = getIndex(checker+1);
             if(nextIdx != 0)
             {
@@ -155,11 +187,11 @@ void insertString(char* input){
                 idxInput++;
             }
             else {
-                printf("checker: %d \n", checker);
+                // printf("checker: %d \n", checker);
                 stop = '1';
             }
         } else{
-            	printf("checker: %d \n", checker);
+            	// printf("checker: %d \n", checker);
 
             	int rightIdx = getIndex(checker+3);
             	while(rightIdx != 0)
@@ -175,7 +207,7 @@ void insertString(char* input){
     int inputSize = (int) strlen(input);
     int i=idxInput;
     insertChar(input[i], checker);
-    for(i=idxInput+1; i<inputSize; i++){
+    for(i=idxInput+1; i<inputSize && isValidTree==1; i++){
         insertChar(input[i], -1);
     }
 }
